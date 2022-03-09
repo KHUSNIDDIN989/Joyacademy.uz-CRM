aa05706dc73be7f106da231b67227685ecf933d4
khusniddin989 <khusniddin989@gmail.com>
2022-03-09 17:32:31 +0500

    sidenav added

--- src/App.js
+++ src/App.js
@@ -1,18 +1,13 @@
 import "./App.css";
-import { Routes, Route } from "react-router-dom";
+
 import Header from "./components/header/Header";
 import PersistentDrawerLeft from "./components/sidenav/SideNaav";
 function App() {
   return (
     <>
-      <Header />
       <PersistentDrawerLeft/>
 
-      <Routes>
-        <Route path="/" />
-        <Route path="expenses" />
-        <Route path="invoices" />
-      </Routes>
+      
     </>
   );
 }
