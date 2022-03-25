567fddb5f297da39f5231518bd87e62a16e22d45
khusniddin989 <khusniddin989@gmail.com>
2022-03-25 21:45:24 +0500

    O'quvchilar tayor

--- src/components/studentes/Studentes.jsx
+++ src/components/studentes/Studentes.jsx
@@ -3,7 +3,7 @@ import SearchIcon from "@mui/icons-material/Search";
 import Pagination from "@mui/material/Pagination";
 import Stack from "@mui/material/Stack";
 import { useEffect, useState } from "react";
-import DeleteIcon from "@mui/icons-material/Delete";
+import Delete from "../../assets/images/delete.png";
 import "./Studentes.css";
 
 function Studentes() {
@@ -14,9 +14,12 @@ function Studentes() {
   const [parentName, setParentName] = useState("");
   const [parentPhone, setParentPhone] = useState("");
   const [file, setFile] = useState();
-  const [hendleDelete, setHendleDelete] = useState(null);
+  const [hendleDelete, setHendleDelete] = useState("");
   const [search, setSearch] = useState("");
   const [page, setPage] = useState(1);
+  const [posts, setPosts] = useState([]);
+
+console.log(students)
 
   useEffect(() => {
     fetch(
@@ -24,10 +27,10 @@ function Studentes() {
     )
       .then((res) => res.json())
       .then((data) => setStudents(data));
-  }, [search, page, hendleDelete]);
-  console.log(students);
+  }, [search, page, posts, hendleDelete]);
+
   console.log(hendleDelete);
-  
+
   const PostForm = (e) => {
     e.preventDefault();
 
@@ -38,35 +41,32 @@ function Studentes() {
     formData.append("parents_phone", parentPhone);
     formData.append("group_id", option);
     formData.append("file", file);
-    formData.append("username", "salom");
-    console.log(file);
 
     fetch(`https://crm-joygroup.herokuapp.com/students`, {
       method: "POST",
       body: formData,
-    }).then((res) => res.json().then((data) => console.log(data)));
+    }).then((res) => res.json().then((data) => setPosts(data)));
 
     // setPhoneNumber(e.target.number.value);
     // setOption(e.target.select.value);
     // setParentName(e.target.parentName.value);
     // setParentPhone(e.target.parentPhone.value);
-    // e.target.text.value = "";
-    // e.target.number.value = "";
-    // e.target.select.value = 1;
-    // e.target.parentName.value = "";
-    // e.target.parentPhone.value = "";
+
+    e.target.reset()
   };
 
-  useEffect(() => {
-    fetch(`https://crm-joygroup.herokuapp.com/students?student_id=${hendleDelete}`, {
+
+  const HendleDelete = (id) => {
+    fetch(`https://crm-joygroup.herokuapp.com/students?student_id=${id}`, {
       method: "DELETE",
     })
       .then((res) => res.json())
-      .then((data) => console.log(data));
-  }, []);
+      .then((data) => setHendleDelete(data));
+  };
 
   const hendleSearch = (e) => {
     e.preventDefault();
+
     setSearch(e.target.search.value);
 
     e.target.search.value = "";
@@ -146,7 +146,6 @@ function Studentes() {
                     name="parentPhone"
                     type="number"
                     className="form-control"
-                    defaultValue="+998 "
                     placeholder="+998 xx xxx xx xx"
                   />
                 </div>
@@ -164,18 +163,14 @@ function Studentes() {
                 </div>
               </div>
               <div className="w-100 m-2 p-3 mt-3 card__btn">
-                <button className="btn btn-primary btn__btn">
-                  To’lov qilish
-                </button>
+                <button className="btn btn-primary btn__btn">Qo’shish</button>
               </div>
             </div>
           </form>
         </div>
 
         <div className="col-md-12 d-flex justify-content-between mt-5 pt-5 mb-3">
-          <h3 className="col__h3">
-            To’lov qilganlar<span>(shu oy bo’yicha)</span>{" "}
-          </h3>
+          <h3 className="col__h3">Bizning o’quvchilar</h3>
           <form onSubmit={(e) => hendleSearch(e)} className="col__img-input">
             <SearchIcon className="col__search" />
             <input
@@ -220,14 +215,17 @@ function Studentes() {
                       <tr key={Math.random()}>
                         <th scope="row">{index + 1}</th>
                         <td>{i.student_name}</td>
-                        <td>{i.student_phone}</td>
+                        <td>+{i.student_phone}</td>
                         <td>{i.group_name}</td>
-                        <td>{i.parents_name} (F.I.SH)</td>
-                        <td>{i.create_student_date}</td>
+                        <td>{i.parents_name}</td>
+                        <td>{i.parents_phone}</td>
                         <td>
-                          <DeleteIcon
-                            onClick={() => setHendleDelete(i.student_id)}
+                          <img
+                            src={Delete}
+                            id={i.student_id}
+                            onClick={(e) => HendleDelete(e.target.id)}
                             className="delete"
+                            alt=""
                           />
                         </td>
                       </tr>
